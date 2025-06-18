692dd84724ae0cd98a91107e28f7d1c7bb3a5a2e
Patience A. Ojerinde <pojerinde@yahoo.com>
2025-06-18 21:02:22 +0100

    Update sitters.js

--- sitters.js
+++ sitters.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://localhost:5000/api";
+const BASE_URL = "https://sits17-com-u1sm-git-master-stephen-s-projects-e6630b6d.vercel.app";
 
 document.addEventListener("DOMContentLoaded", function () {
 // Book buttons (for multiple sitters)
